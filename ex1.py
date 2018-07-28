e1ae916801b66abfa0a8de01164907df920fbdfd
xmh <18001998506@163.com>
2018-07-28 09:50:31 +0800

    This is my first python's exercise in summer school in 2018

---
+++ ex1.py
@@ -0,0 +1,9 @@
+# Tab width:4 spaces
+# insert spaces instead of tabs
+# Automatic indentation
+print "Hello World!",  # comma can make the follow line print 
+                       # the same line with this line
+print "I am a machine."
+print("Hello World! I am a machine.")
+
+
